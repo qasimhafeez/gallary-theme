8dfb087ab25ef036974aaef94a9819b5abfa8f89
Qasim Hafeez <qasimh556@gmail.com>
2020-01-14 11:27:03 +0400

    Gallary Support

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function theme_setup(){
     // Feature Image support to theme
     add_theme_support('post-thumbnails');
-    add_post_thumbnail_size(900, 600);
+    set_post_thumbnail_size(900, 600);
 
     // Add theme gallary support
     add_theme_support('post-formats', array('gallary'));
