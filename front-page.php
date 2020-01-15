f98416309148c0ca69380c6e258d1ca8dd597bfe
Qasim Hafeez <qasimh556@gmail.com>
2020-01-15 14:10:07 +0400

    changed to posts page

--- front-page.php
+++ front-page.php
@@ -1,50 +1,12 @@
 <?php get_header(); ?>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    <div class='col-md-4'>
-      <div class='thumbnail'>
-        <a href=""><img class='img-responsive' src='http://placehold.it/450x250' alt=''></a>
-      </div>
-    </div>
-    
+<?php if(have_posts()) : ?>
+    <?php while(have_posts()) : the_post(); ?>
+        <?php get_template_part('content', get_post_format()); ?>
+    <?php endwhile; ?>
+<?php else: ?>
+    <?php echo wpautop('No post available!'); ?>
+<?php endif; ?>
+
   </div>
 </div>
 <?php get_footer(); ?>
