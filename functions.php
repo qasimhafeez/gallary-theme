605dc789dd44d9685c3c4768e367257d6fcec9cb
Qasim Hafeez <qasimh556@gmail.com>
2020-01-15 12:27:33 +0400

    Added custom css to link and menus

--- functions.php
+++ functions.php
@@ -32,6 +32,19 @@ register_nav_menus(
     )
 );
 
+// Adding Menu Classes
+function menu_item_class($classes, $item) {       
+    $classes[] = "nav-item";
+    $classes[] = "p-1"; 
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'menu_item_class', 10, 2);
+
+// Custom Menu Links Class
+function modify_menuclass($ulclass) {
+    return preg_replace('/<a /', '<a class="nav-link btn btn-primary btn-sm"', $ulclass);
+}
+add_filter('wp_nav_menu', 'modify_menuclass');
 
 
 // Hooks
