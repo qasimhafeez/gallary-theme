6f358e1f4bc75f684e0fdc971ccf3d0c3e6c95a6
Qasim Hafeez <qasimh556@gmail.com>
2020-01-19 15:41:00 +0400

    AJAX Page Loading

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function($) {
+  var $content = $("#content");
+  var URL = "";
+  var siteURL = "http://" + top.location.host.toString();
+  var $internalLinks = $("a[href^='" + siteURL + "']");
+  var hash = window.location.hash;
+  var $el,
+    allLinks = $("a");
+  console.log(hash);
+
+  $internalLinks
+    .each(function() {
+      $(this).attr("href", "#" + this.pathname);
+    })
+    .click(function() {
+      $el = $(this);
+      URL = $el.attr("href").substring(1);
+      URL = URL + "#inside";
+
+      $content.load(URL, function() {});
+    });
+})(jQuery);
